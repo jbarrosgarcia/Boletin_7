83cbfd7fcfd65cbca3c6e132e37ea0edcaa2412a
jbarrosgarcia <jbarrosgarcia@atanasoff07.danielcastelao.org>
2015-11-03 08:55:16 +0100

    Cambie el funcionamiento del programa

---
+++ Boletin7_1/src/boletin7_1/Boletin7_1.java
@@ -0,0 +1,12 @@
+
+package boletin7_1;
+import javax.swing.JOptionPane;
+
+public class Boletin7_1 {
+
+    public static void main(String[] args) {
+        int num1=Metodos.pedirNumeroEnteiro();
+       System.out.println(Metodos.compararPos(num1));
+    }
+    
+}
